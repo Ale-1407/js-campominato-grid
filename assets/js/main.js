0c8a2f28020ad719efccfc114c43c7dedb4185f1
Ale-1407 <alessiacarrera07@gmail.com>
2022-11-02 09:19:48 +0100

    esercizio completato senza bonus

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,30 @@ Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro
 ed emetto un messaggio in console con il numero della cella cliccata.
 */
 
+const griglia = document.getElementById("griglia");
+const button = document.getElementById("button");
+
+//creo quadrati dentro la griglia
+
+function creazionequadrati(){
+    const div = document.createElement("div");
+    div.classList.add("quadrato");
+    return div;
+}
+
+button.addEventListener('click', function(){
+   for( let i = 1; i <= 100; i++ ) {
+
+    let elementocorrente = creazionequadrati();
+
+    elementocorrente.addEventListener('click', function() {
+        this.classList.toggle('active');
+    })
+
+    elementocorrente.innerHTML = i;
+    elementocorrente.classList.add('text-center');
+
+    griglia.append(elementocorrente);
+   }
+})
+
